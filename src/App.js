593db15f0e931d681073e49f4c81a8f359e0cbb3
Michael Warren <mwarrn@amazon.com>
2022-06-18 19:11:19 -0700

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  // when App component renders, also update the document's title
+  // but *only* do that when the text variable changes
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    document.title = text;
+  }, [text]);
+
+  // when App component renders, set this timeout function
+  // in 5 seconds, update state and set the count back to 0
+  // only set this timeout function once, not every time the component updates
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+  }, []);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>
+      <input
+        type="text"
+        placeholder="Type away..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </div>
+  );
 }
 
 export default App;
